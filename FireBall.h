e61260a833f86ba2b51e01ab0f3388b13b4da417
Yoppy <beretta92f.104@gmail.com>
2018-12-16 16:20:42 +0900

    "マップ関係のもろもろ"

---
+++ FireBall.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Magic.h"
+#include "Player.h"
+#include "Enemy.h"
+
+typedef unsigned long long stagedata;
+
+class FireBall : public Magic {
+private:
+	int type;
+	int damage;
+	int attackable;//“¯S‰~‚Ì”ÍˆÍA-1‚Ì‚Í‘S”ÍˆÍ
+	int attack_area;//UŒ‚”ÍˆÍ
+	virtual void effectpoint(int, Player**,int, Enemy**, int);
+public:
+	FireBall();
+	virtual bool effectBattle(stagedata, Player**, int, int, Enemy**, int);
+	virtual void effectMap();
+};
